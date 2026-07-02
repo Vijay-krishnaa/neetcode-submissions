183ff0a41369a34720faf9a46b96127b7eae9769
Vijay-krishnaa <82525126+Vijay-krishnaa@users.noreply.github.com>
2026-07-02 21:03:37 +0530

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int trap(int[] h) {
+        int n = h.length;
+        int water = 0;
+
+        for(int i = 0;i<n-1;i++){
+            int rmax = h[i];
+            int lmax = h[i];
+
+        for(int j = 0; j <= i ; j++){
+            lmax = Math.max(lmax, h[j]);
+        }
+         for(int k = i; k < n ; k++){
+            rmax = Math.max(rmax, h[k]);
+        }
+
+     water += Math.min(rmax,lmax) - h[i];
+
+
+
+
+        }
+return water;
+       
+    }
+}
